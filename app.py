cfe8c1c4f74464e9641127167164c2e864dafec3
akash-r-prabhu <73175408+akash-r-prabhu@users.noreply.github.com>
2021-10-29 20:10:38 +0530

    75% functional have to add pdf

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import re
 import random
 from flask import Flask,render_template,request
+from flask.sessions import NullSession
 from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']="sqlite:///users_db.db"
@@ -26,8 +27,10 @@ def hello_world():
 
 @app.route("/user_registration",methods=['GET','POST'])
 def user_registration():
+    ruid=0
     if request.method=="POST":
         uid=random.randint(1000,9999)
+        ruid=uid
         upass=request.form['pass']
         ufname=request.form['fname']
         ulname=request.form['lname']
@@ -36,15 +39,50 @@ def user_registration():
         users = users_db(uid=uid,ufname=ufname,ulname=ulname,umobile=umobile,uaadhar=uaadhar,upass=upass)
         db.session.add(users)
         db.session.commit()
-    return render_template("user_registration.html")
+    return render_template("user_registration.html",ruid=ruid)
 
-@app.route("/user_login")
+@app.route("/user_login",methods=['GET','POST'])
 def user_login():
+    if request.method=="POST":
+        uid=request.form['uid']
+        upass=request.form['pass']
+        user= users_db.query.filter_by(uid=uid).first()
+        if(user is None):
+            return '''<head><link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"></head><a href='/user_login'><button class="btn btn-warning" style="padding:30px;margin:20px">Try again</button></a><script>function f(){alert('Entered username/password is incorrect please try again')}; f();</script>'''
+        if(user.upass==upass):
+            return render_template("user_homepage.html")
+            
     return render_template("user_login.html")
 
 @app.route("/hospital_login")
 def hospital_login():
     return render_template("hospital_login.html")
+
+@app.route("/book_appointments")
+def appointment_details():
+    return render_template("appointment_details.html")
+
+@app.route("/user_homepage")
+def user_homepage():
+    return render_template("user_homepage.html")
+
+@app.route("/hospital_homepage")
+def hospital_homepage():
+    return render_template("hospital_homepage.html")
+
+@app.route("/request_for_blood")
+def request_for_blood():
+    return "<h1>This is under development</h1>"
+
+@app.route("/patient_search",methods=['GET','POST'])
+def patient_search():
+    usr={}
+    if request.method=="POST":
+        name=request.form['name']
+        users= users_db.query.filter_by(ufname=name)
+        # users= users_db.query.all()
+        return render_template("patient_search.html",users=users)
+    return render_template("patient_search.html")
 
 @app.route("/show")
 def show():
@@ -52,9 +90,6 @@ def show():
     print(allUsers)
     return "<p>This is show page</p>"
 
-@app.route("/products")
-def products():
-    return "<p>This is products page</p>"
 
 if __name__=="__main__":
     app.run(debug=True)
